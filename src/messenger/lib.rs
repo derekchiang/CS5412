dbacf3fa75f679f95eba62e899485e9b4d53afd8
Derek Chiang <derekchiang93@gmail.com>
2014-02-13 01:32:21 -0500

    Add some skeleton

--- src/messenger/lib.rs
+++ src/messenger/lib.rs
@@ -3,6 +3,24 @@
 #[crate_type = "dylib"];
 #[crate_type = "rlib"];
 
+pub trait HasAddress {
+    fn get_address() -> SocketAddr;
+}
+
+pub struct Messenger {
+
+}
+
+impl Messenger {
+    pub fn send<T: HasAddress, 'a, U: Encodable<json::Encoder<'a>>>(&mut self, to: T, msg: U) {
+
+    }
+
+    pub fn recv<'a, U: Decodable<json::Decoder>>(&mut self) -> U {
+
+    }
+}
+
 pub fn hello() {
     println!("Hello World!");
 }
