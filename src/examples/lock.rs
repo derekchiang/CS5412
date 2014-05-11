96bc1a9b9ebbec0678273d4859ae8da81aa72855
Derek Chiang <derekchiang93@gmail.com>
2014-05-11 19:12:29 -0400

    Update lock example

--- src/examples/lock.rs
+++ src/examples/lock.rs
@@ -3,6 +3,7 @@
 extern crate paxos;
 extern crate collections;
 extern crate serialize;
+extern crate rand;
 
 use std::io::timer::sleep;
 
@@ -11,6 +12,8 @@ use collections::hashmap::HashMap;
 use paxos::StateMachine;
 use paxos::Command;
 use paxos::ServerID;
+
+use rand::random;
 
 struct Lock<T> {
     holder: Option<ServerID>,
@@ -83,27 +86,48 @@ impl StateMachine<Response> for Lock<Response> {
     }
 }
 
+// Sleep for any time between 0 and 1 second
+fn random_sleep() {
+    sleep(random::<u64>() % 1000);
+}
+
 fn main() {
     let cluster = paxos::new_cluster::<Response, Lock<Response>>();
 
     let client1 = cluster.new_client(7u64 << 32);
     let client2 = cluster.new_client(8u64 << 32);
+    let client3 = cluster.new_client(9u64 << 32);
 
     spawn(proc() {
         let mut client1 = client1;
-        let res_rx = client1.call(~"lock", vec!());
-        println!("Client 1: {}", res_rx.recv());
-        sleep(2000);
-        let res_rx = client1.call(~"unlock", vec!());
-        println!("Client 1: {}", res_rx.recv());
+        for _ in range(0, 10) {
+            let res_rx = client1.call(~"lock", vec!());
+            println!("Client 1: {}", res_rx.recv());
+            random_sleep();
+            let res_rx = client1.call(~"unlock", vec!());
+            println!("Client 1: {}", res_rx.recv());
+        }
     });
 
     spawn(proc() {
         let mut client2 = client2;
-        let res_rx = client2.call(~"lock", vec!());
-        println!("Client 2: {}", res_rx.recv());
-        sleep(2000);
-        let res_rx = client2.call(~"unlock", vec!());
-        println!("Client 2: {}", res_rx.recv());
+        for _ in range(0, 10) {
+            let res_rx = client2.call(~"lock", vec!());
+            println!("Client 2: {}", res_rx.recv());
+            random_sleep();
+            let res_rx = client2.call(~"unlock", vec!());
+            println!("Client 2: {}", res_rx.recv());
+        }
+    });
+
+    spawn(proc() {
+        let mut client3 = client3;
+        for _ in range(0, 10) {
+            let res_rx = client3.call(~"lock", vec!());
+            println!("Client 3: {}", res_rx.recv());
+            random_sleep();
+            let res_rx = client3.call(~"unlock", vec!());
+            println!("Client 3: {}", res_rx.recv());
+        }
     });
 }
